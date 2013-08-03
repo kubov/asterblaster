48c89bd7585e5c384dbd46ef25b28ae567875767
Danka Niedzialkowska <danuta.niedzialkowska@gmail.com>
2013-08-03 18:45:47 +0200

    Spaceship and asteroid improved

--- frontend/js/asteroids.js
+++ frontend/js/asteroids.js
@@ -1,6 +1,7 @@
 var SPACESHIP_RADIUS = 15;
 var SPACESHIP_SIDES = 3;
 var PROJECTILE_RADIUS = 2;
+var STROKE_WIDTH = 2;
 
 function Asteroid(x, y, size) {
     this.x = x;
@@ -11,18 +12,22 @@ function Asteroid(x, y, size) {
 Asteroid.prototype.draw = function() {
     new Circle(this.x, this.y, this.size)
         .addTo(stage)
-        .attr('fillColor', 'white')
+        .attr({strokeColor: 'white',
+               strokeWidth: STROKE_WIDTH})
 };
 
-function Spaceship(x, y) {
+function Spaceship(x, y, rot) {
     this.x = x;
     this.y = y;
+    this.rot = rot;
 };
 
 Spaceship.prototype.draw = function() {
     new Polygon(this.x, this.y, SPACESHIP_RADIUS, SPACESHIP_SIDES)
         .addTo(stage)
-        .attr('fillColor', 'white');
+        .attr({strokeColor: 'white',
+               strokeWidth: STROKE_WIDTH,
+               rotation: this.rot});
 };
 
 function Projectile(x, y) {
@@ -37,12 +42,12 @@ Projectile.prototype.draw = function() {
 }
 
 function drawState(arr) {
-    for (i = 0; i < 3; ++i) {
+    for (i = 0; i < arr.length; ++i) {
         arr[i].draw();
     }
 };
 
-bonsai.Path.rect(0, 0, 800, 1900).attr({fillColor:'black'}).addTo(stage);
+bonsai.Path.rect(0, 0, 700, 1900).attr({fillColor:'black'}).addTo(stage);
 
-var arr = [new Asteroid(200, 136, 50), new Spaceship(20, 20), new Projectile(80, 80)];
+var arr = [new Asteroid(200, 136, 50), new Spaceship(20, 20, Math.PI/180*35), new Projectile(80, 80)];
 drawState(arr);
