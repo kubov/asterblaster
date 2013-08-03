71ad81526b63706e97d9c8f2ddc7154344b81d90
Adam Michalik <dodek@sniezynka.thefacebook.com>
2013-08-03 21:53:58 +0200

    server.lisp added

---
+++ backend/server.lisp
@@ -0,0 +1,33 @@
+(in-package 'asterblaster)
+
+(setf clws:*debug-on-server-errors* t)
+(setf clws:*debug-on-resource-errors* t)
+
+
+(bordeaux-threads:make-thread (lambda ()
+                                (run-server 13373))
+                              :name "Asterblaster")
+
+(defclass api-resource (ws-resource)
+  ())
+
+(defmethod resource-received-text ((res api-resource) client message)
+  (write-to-client-text client message))
+
+(defmethod resource-client-connected ((res api-resource) client)
+  (format t "[connection on api server from ~s : ~s]~%"
+          (client-host client) (client-port client))
+  t)
+
+(defmethod resource-client-disconnected ((resource api-resource) client)
+  (format t "[disconnected from resource ~A: ~A]~%" resource client))
+
+
+(register-global-resource "/api"
+                          (make-instance 'api-resource)
+                          (origin-prefix nil))
+
+(bordeaux-threads:make-thread (lambda ()
+                                (run-resource-listener
+                                 (find-global-resource "/api")))
+                              :name "resource listener for /api")
