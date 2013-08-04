90597aff291f46b04fda30f1b9ca4bf4adc47b56
Magda <a@a.pl>
2013-08-04 04:02:58 +0100

    WebSocket basics

---
+++ frontend/js/settings.js
@@ -0,0 +1,13 @@
+// Control object settings
+
+// arrows control
+var MOVE_UP = 38;
+var MOVE_DOWN = 40;
+var MOVE_LEFT = 37;
+var MOVE_RIGHT = 39;
+
+var SHOT = 32; // space
+
+
+// Connection settings
+var SERVER_URL = "ws://54.213.111.220:13373/api";
